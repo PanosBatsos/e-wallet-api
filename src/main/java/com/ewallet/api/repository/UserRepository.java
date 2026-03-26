9698be95aea92fd7e30e8c09ea5455ab3bf59021
Panos Batsos <panagiotismpatsos@gmail.com>
2026-03-26 21:41:04 +0200

    feat(UserRepository): add findByEmail to check if user already exists

--- src/main/java/com/ewallet/api/repository/UserRepository.java
+++ src/main/java/com/ewallet/api/repository/UserRepository.java
@@ -1,5 +1,7 @@
 package com.ewallet.api.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,5 @@ import com.ewallet.api.entity.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User , Long>{
-
+    Optional<User> findByEmail(String email); // checking if this user already exists
 } 
