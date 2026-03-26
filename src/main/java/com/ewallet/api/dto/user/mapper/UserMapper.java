8e4bac8fda2a0505bcabc265943e1f0406941eaf
Panos Batsos <panagiotismpatsos@gmail.com>
2026-03-26 02:54:14 +0200

    feat(UserMapper) add UserMapper

---
+++ src/main/java/com/ewallet/api/dto/user/mapper/UserMapper.java
@@ -0,0 +1,39 @@
+package com.ewallet.api.dto.user.mapper;
+
+import org.springframework.stereotype.Component;
+
+import com.ewallet.api.dto.user.UserRegisterRequestDTO;
+import com.ewallet.api.dto.user.UserResponseDTO;
+import com.ewallet.api.entity.User;
+
+@Component
+public class UserMapper {
+
+    public User toUser(UserRegisterRequestDTO dto) {
+        return User.builder()
+            .email(dto.getEmail())
+            .password(dto.getPassword()) 
+            .firstName(dto.getFirstName())
+            .lastName(dto.getLastName())
+            .birthDate(dto.getBirthDate())
+            .idCardNumber(dto.getIdCardNumber())
+            .taxNumber(dto.getTaxNumber())
+            .build();
+    }
+
+
+    public UserResponseDTO toDTO (User user) {
+        if (user.getWallet() == null) {
+            throw new RuntimeException("User has not been mapped to a wallet");
+        }
+
+        return UserResponseDTO.builder()
+            .email(user.getEmail())
+            .firstName(user.getFirstName())
+            .id(user.getId())
+            .birthDate(user.getBirthDate())
+            .walletId(user.getWallet().getId())
+            .lastName(user.getLastName())
+            .build();
+    }
+}
