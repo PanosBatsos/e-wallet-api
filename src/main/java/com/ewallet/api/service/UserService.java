8d8a444a94108b9c1f6755d090f737d56b9c8fc9
Panos Batsos <panagiotismpatsos@gmail.com>
2026-03-27 05:46:01 +0200

    docs(UserService) add comments

--- src/main/java/com/ewallet/api/service/UserService.java
+++ src/main/java/com/ewallet/api/service/UserService.java
@@ -15,26 +15,37 @@ import com.ewallet.api.entity.User;
 import com.ewallet.api.entity.Wallet;
 import com.ewallet.api.repository.UserRepository;
 
+
 import lombok.RequiredArgsConstructor;
 
 @Service
-@RequiredArgsConstructor
+@RequiredArgsConstructor // For automatic DI
 public class UserService {
     
     private final UserRepository userRepository;
     private final UserMapper userMapper;
     private final PasswordEncoder passwordEncoder;
-
     
-    @Transactional
+    /**
+     * Registers a new user and automatically initializes their e-wallet
+     * @param dto The data transfer object containing reg details(firstname , lastname , email 
+     * , password , currency , dob , id card's number , taxnumber)
+     * @return UserResponseDTO 
+     * @throws RuntimeException (for now) if the provided email is already registered in the db 
+     */
+
+    @Transactional // Ensures that the user and wallet creation happen as a single atomic operation
     public UserResponseDTO registerUser(UserRegisterRequestDTO dto){
+        // Check if the email is already in use
         if (userRepository.findByEmail(dto.getEmail()).isPresent()) {
             throw new RuntimeException("This user already exist");
         }
 
+        // Mapping and encoding the password
         User user = userMapper.toUser(dto);
         user.setPassword(passwordEncoder.encode(dto.getPassword()));
-    
+        
+        // Build the wallet for the user  
         Wallet wallet = Wallet.builder()
         .balance(BigDecimal.ZERO)
         .currency(dto.getCurrency())
@@ -44,8 +55,10 @@ public class UserService {
 
         user.setWallet(wallet);
         
+        // Save the user (wallet is saved automatically due to cascade)
         User savedUser = userRepository.save(user);
 
+        // Response
         return userMapper.toDTO(savedUser);
     }
 }
