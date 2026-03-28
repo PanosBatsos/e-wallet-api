215403b239309f70657fef5b647c6a06430b2975
Panos Batsos <panagiotismpatsos@gmail.com>
2026-03-28 12:44:58 +0200

    refactor(WalletService): return response DTO from wallet deposit

--- src/main/java/com/ewallet/api/service/WalletService.java
+++ src/main/java/com/ewallet/api/service/WalletService.java
@@ -2,9 +2,12 @@ package com.ewallet.api.service;
 
 
 
+import java.time.LocalDateTime;
+
 import org.springframework.stereotype.Service;
 
 import com.ewallet.api.dto.wallet.WalletDepositRequestDTO;
+import com.ewallet.api.dto.wallet.WalletDepositResponseDTO;
 import com.ewallet.api.entity.User;
 import com.ewallet.api.entity.Wallet;
 import com.ewallet.api.repository.UserRepository;
@@ -24,12 +27,12 @@ public class WalletService {
      * Validates user existence and ensures currency compability
      * @param dto The deposit request
      * @throws RuntimeException if the user is not found if there is a currency mismatch 
-     *  
+     *  @return a deposit response dto
      * Exception handling to be added in the future and transaction logic
      */
 
     @Transactional
-    public void deposit(WalletDepositRequestDTO dto) {
+    public WalletDepositResponseDTO deposit(WalletDepositRequestDTO dto) {
         // Retrive the user from the database
         User user = userRepository.findById(dto.getUserId())
                 .orElseThrow(() -> new RuntimeException("User not found"));   
@@ -45,5 +48,15 @@ public class WalletService {
         wallet.setBalance(wallet.getBalance().add(dto.getAmount()));
 
         // Transaction will be added later...
+
+
+
+        return WalletDepositResponseDTO.builder()
+            .amountDeposited(dto.getAmount())
+            .currency(dto.getCurrency())
+            .message("Deposit completed")
+            .newBalance(wallet.getBalance())
+            .timestamp(LocalDateTime.now())
+            .build();
     }
 }
