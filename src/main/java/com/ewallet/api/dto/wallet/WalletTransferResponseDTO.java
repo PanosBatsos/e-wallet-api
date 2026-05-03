16d4b9a5e5914e8c27fbe39ac33db4cd87b3e634
Panos Batsos <panagiotismpatsos@gmail.com>
2026-05-04 02:01:15 +0300

    feat(WalletService): add inital transfer logic

--- src/main/java/com/ewallet/api/dto/wallet/WalletTransferResponseDTO.java
+++ src/main/java/com/ewallet/api/dto/wallet/WalletTransferResponseDTO.java
@@ -1,11 +1,19 @@
 package com.ewallet.api.dto.wallet;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class WalletTransferResponseDTO {
-    private BigDecimal amountDeposited;
-    private BigDecimal newBalance;
+    private BigDecimal amountTransferred;
     private String currency;
     private LocalDateTime timestamp;
     private String description;
--- src/main/java/com/ewallet/api/service/WalletService.java
+++ src/main/java/com/ewallet/api/service/WalletService.java
@@ -2,11 +2,15 @@ package com.ewallet.api.service;
 
 
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
+import com.ewallet.api.dto.wallet.WalletTransferRequestDTO;
+import com.ewallet.api.dto.wallet.WalletTransferResponseDTO;
 import com.ewallet.api.exception.CurrencyMismatchException;
 import com.ewallet.api.exception.ResourceNotFoundException;
 import com.ewallet.api.service.kafka.TransactionProducer;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.ewallet.api.dto.wallet.WalletDepositRequestDTO;
@@ -26,7 +30,8 @@ public class WalletService {
     
     private final UserRepository userRepository;
     private final TransactionService transactionService;
-    
+
+
     /**
      * Processes a deposit request for a specific user
      * Validates user existence and ensures currency compability.
@@ -71,5 +76,72 @@ public class WalletService {
             .newBalance(wallet.getBalance())
             .timestamp(LocalDateTime.now())
             .build();
+    }
+
+
+    /**
+     * Processes a fund transfer between two users' wallets
+     * Validates user existence, sufficient funds and currency compatibility
+     *
+     * The current transaction recording is synchronous and serves
+     * as a baseline for testing security and business logic
+     * In the future this will be refactored to publish TransactionEvents
+     * to Kafka for asynchronous processing and event-driven architecture
+     *
+     * @param dto The transfer request payload
+     * @param senderEmail The email of the authenticated user initiating the transfer
+     * @return WalletTransferResponseDTO containing the transfer details
+     */
+
+
+    @Transactional
+    public WalletTransferResponseDTO transfer(WalletTransferRequestDTO dto , String senderEmail) {
+
+        // Check for sender's and receiver's existence
+        User sender = userRepository.findByEmail(senderEmail)
+                .orElseThrow(() -> new ResourceNotFoundException("This user does not exist"));
+
+        User receiver = userRepository.findByEmail(dto.getReceiverEmail())
+                .orElseThrow(() -> new ResourceNotFoundException("This user does not exist"));
+
+        BigDecimal senderBalance = sender.getWallet().getBalance();
+
+        // Check for insufficient balance
+        if (senderBalance.compareTo(dto.getAmount()) < 0) {
+            // InsufficientBalanceException will be added in the future
+            throw new RuntimeException("Insufficient funds for this transfer");
+        }
+
+        Wallet senderWallet = sender.getWallet();
+        Wallet receiverWallet = receiver.getWallet();
+
+        // Check for currency mismatch
+        if (!senderWallet.getCurrency().equals(receiverWallet.getCurrency())) {
+            throw new CurrencyMismatchException("Cannot transfer between different currencies");
+        }
+
+        // Sync for now
+        senderWallet.setBalance(senderWallet.getBalance().subtract(dto.getAmount()));
+        receiverWallet.setBalance(receiverWallet.getBalance().add(dto.getAmount()));
+
+        transactionService.recordTransaction(senderWallet,
+                TransactionType.TRANSFER, // Consider using TRANSFER_ΟUT in the future
+                dto.getAmount(),
+         dto.getDescription());
+
+        transactionService.recordTransaction(receiverWallet,
+                TransactionType.TRANSFER, // Consider using TRANSFER_IN in the future
+                dto.getAmount(),
+                dto.getDescription());
+
+        return  WalletTransferResponseDTO.builder()
+                .senderEmail(senderEmail)
+                .receiverEmail(receiver.getEmail())
+                .description(dto.getDescription())
+                .amountTransferred(dto.getAmount())
+                .timestamp(LocalDateTime.now())
+                .currency(dto.getCurrency())
+                .build();
+                
     }
 }
