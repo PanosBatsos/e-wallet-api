6bf67f8edeffff3b565aee12d2d1c480911cbbaa
Panos Batsos <panagiotismpatsos@gmail.com>
2026-05-03 18:29:33 +0300

    feat(AuthController): implement cotroller with register and login endpoints

---
+++ src/main/java/com/ewallet/api/controller/AuthenticationController.java
@@ -0,0 +1,34 @@
+package com.ewallet.api.controller;
+
+import com.ewallet.api.dto.user.AuthenticationResponse;
+import com.ewallet.api.dto.user.UserLoginRequestDTO;
+import com.ewallet.api.dto.user.UserRegisterRequestDTO;
+import com.ewallet.api.service.AuthenticationService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v0/auth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+    private final AuthenticationService authenticationService;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(@Valid @RequestBody UserRegisterRequestDTO dto) {
+        AuthenticationResponse authenticationResponse = authenticationService.register(dto);
+        return new ResponseEntity<>(authenticationResponse , HttpStatus.CREATED);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthenticationResponse> login(@Valid @RequestBody UserLoginRequestDTO dto) {
+        AuthenticationResponse authenticationResponse = authenticationService.login(dto);
+        return new ResponseEntity<>(authenticationResponse , HttpStatus.OK);
+    }
+
+}
