b5cabf35ba70244895674334dde3064e93f5d4c7
Panos Batsos <panagiotismpatsos@gmail.com>
2026-05-03 17:01:31 +0300

    feat(AuthService): implement the service with register method

---
+++ src/main/java/com/ewallet/api/service/AuthenticationService.java
@@ -0,0 +1,79 @@
+package com.ewallet.api.service;
+
+import com.ewallet.api.dto.user.AuthenticationResponse;
+import com.ewallet.api.dto.user.UserRegisterRequestDTO;
+import com.ewallet.api.entity.User;
+import com.ewallet.api.entity.Wallet;
+import com.ewallet.api.exception.UserAlreadyExistsException;
+import com.ewallet.api.repository.UserRepository;
+import com.ewallet.api.security.JwtService;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
+
+    /**
+     * Registers a new user and automatically provisions an initial wallet for them
+     * @param userRegisterRequestDTO The DTO containing registration details
+     * @return AuthenticationResponse containing the token for the newly registered user
+     */
+    @Transactional
+    public AuthenticationResponse register(UserRegisterRequestDTO userRegisterRequestDTO) {
+
+        // Check if request's email is already used
+        if (userRepository.existsByEmail(userRegisterRequestDTO.getEmail())) {
+            throw new UserAlreadyExistsException("A user with this email already exists.");
+        }
+
+        // Check if request's Tax Num is already used
+        if (userRepository.existsByTaxNumber(userRegisterRequestDTO.getTaxNumber())) {
+            throw new UserAlreadyExistsException("A user with this Tax Number (AFM) already exists.");
+        }
+
+        // Check if request's id card number is already used
+        if (userRepository.existsByIdCardNumber(userRegisterRequestDTO.getIdCardNumber())) {
+            throw new UserAlreadyExistsException("A user with this ID Card Number already exists.");
+        }
+        // Build the User entity
+        User user = User.builder()
+                .firstName(userRegisterRequestDTO.getFirstName())
+                .lastName(userRegisterRequestDTO.getLastName())
+                .email(userRegisterRequestDTO.getEmail())
+                .password(passwordEncoder.encode(userRegisterRequestDTO.getPassword()))
+                .idCardNumber(userRegisterRequestDTO.getIdCardNumber())
+                .taxNumber(userRegisterRequestDTO.getTaxNumber())
+                .birthDate(userRegisterRequestDTO.getBirthDate())
+                .build();
+
+        // Provision a new wallet with a default balance of zero
+        Wallet wallet = Wallet.builder()
+                .balance(BigDecimal.ZERO)
+                .currency(userRegisterRequestDTO.getCurrency())
+                .user(user)
+                .build();
+
+        // Establish the bidirectional relationship
+        // This is required for hibernate to correctly cascade the save operation to the wallet
+        user.setWallet(wallet);
+
+        // Persist the user the wallet is saved automatically due to CascadeType.ALL.
+        userRepository.save(user);
+
+        String jwtToken = jwtService.generateToken(user);
+
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+}
