4e25230032f6b272d497998cea035d925d161b1b
Panos Batsos <panagiotismpatsos@gmail.com>
2026-05-03 16:55:25 +0300

    feat(UserRepository): add check queries for unique constraints (email, AFM, ID)

--- src/main/java/com/ewallet/api/repository/UserRepository.java
+++ src/main/java/com/ewallet/api/repository/UserRepository.java
@@ -10,4 +10,8 @@ import com.ewallet.api.entity.User;
 @Repository
 public interface UserRepository extends JpaRepository<User , Long>{
     Optional<User> findByEmail(String email); // checking if this user already exists
+
+    boolean existsByEmail(String email);
+    boolean existsByTaxNumber(String taxNumber);
+    boolean existsByIdCardNumber(String idCardNumber);
 } 
