43b9e4bb879204562f3e8aeaae92a91766a98b30
Panos Batsos <panagiotismpatsos@gmail.com>
2026-05-09 15:53:41 +0300

    feat(TransactionService): add initial transaction logic for all types

--- src/main/java/com/ewallet/api/service/TransactionService.java
+++ src/main/java/com/ewallet/api/service/TransactionService.java
@@ -5,6 +5,7 @@ import java.time.LocalDateTime;
 
 
 import com.ewallet.api.dto.kafka.TransactionEvent;
+import com.ewallet.api.dto.transaction.TransactionResponseDTO;
 import com.ewallet.api.service.kafka.TransactionProducer;
 import org.springframework.stereotype.Service;
 
@@ -23,38 +24,115 @@ public class TransactionService {
     private final TransactionRepository transactionRepository;
     private final TransactionProducer transactionProducer;
 
-    // Records a transaction and notifies the Kafka broker.
-    // Currently supports only deposit. The other types will be added in the future.
     @Transactional
-    public Transaction recordTransaction(Wallet wallet , TransactionType transactionType , BigDecimal amount , String description) {
-        Transaction transaction = new Transaction();
+    public TransactionResponseDTO recordDeposit(Wallet wallet , BigDecimal amount , String description , String userEmail) {
+        Transaction transaction = Transaction.builder()
+                .amount(amount)
+                .status(TransactionStatus.SUCCESS) // For now
+                .sourceWallet(null)
+                .destinationWallet(wallet)
+                .description(description)
+                .type(TransactionType.DEPOSIT)
+                .build();
 
-        if (transactionType.equals(TransactionType.DEPOSIT)) {
-            transaction.setAmount(amount);
-            transaction.setDestinationWallet(wallet);
-            transaction.setDescription(description);
-            transaction.setSourceWallet(null);
-            transaction.setType(transactionType);
-            transaction.setStatus(TransactionStatus.SUCCESS); // (Sync for now) async logic to be added in the future
-            Transaction savedTransaction = transactionRepository.save(transaction);
+        Transaction savedTransaction = transactionRepository.save(transaction);
 
-            TransactionEvent event = TransactionEvent.builder()
-                    .transactionId(savedTransaction.getId())
-                    .type(savedTransaction.getType().toString())
-                    .sourceWalletId(null)
-                    .destinationWalletId(wallet.getId())
-                    .amount(amount)
-                    .currency(wallet.getCurrency())
-                    .description(description)
-                    .timestamp(LocalDateTime.now())
-                    .build();
+        TransactionEvent transactionEvent = TransactionEvent.builder()
+                .transactionId(savedTransaction.getId())
+                .sourceWalletId(null)
+                .destinationWalletId(wallet.getId())
+                .type(TransactionType.DEPOSIT.toString())
+                .amount(amount)
+                .currency(wallet.getCurrency())
+                .description(description)
+                .timestamp(savedTransaction.getTimestamp())
+                .build();
 
-            transactionProducer.sendTransactionEvent(event);
-            return savedTransaction;
-        }
-        // Other transaction cases will be added here in the future
-        throw new UnsupportedOperationException("Transaction type not supported yet");
+        transactionProducer.sendTransactionEvent(transactionEvent);
+
+        return TransactionResponseDTO.builder()
+                .transactionId(savedTransaction.getId())
+                .senderEmail(null)
+                .receiverEmail(userEmail)
+                .amount(savedTransaction.getAmount())
+                .status(savedTransaction.getStatus())
+                .type(savedTransaction.getType())
+                .timestamp(savedTransaction.getTimestamp())
+                .build();
     }
 
+    @Transactional
+    public TransactionResponseDTO recordTransfer(Wallet sourceWallet , Wallet destinationWallet , BigDecimal amount , String description , String senderEmail , String receiverEmail) {
+        Transaction transaction = Transaction.builder()
+                .amount(amount)
+                .status(TransactionStatus.SUCCESS) // For now
+                .sourceWallet(sourceWallet)
+                .destinationWallet(destinationWallet)
+                .description(description)
+                .type(TransactionType.TRANSFER)
+                .build();
+
+        Transaction savedTransaction = transactionRepository.save(transaction);
+
+        TransactionEvent transactionEvent = TransactionEvent.builder()
+                .transactionId(savedTransaction.getId())
+                .sourceWalletId(sourceWallet.getId())
+                .destinationWalletId(destinationWallet.getId())
+                .type(TransactionType.TRANSFER.toString())
+                .amount(amount)
+                .currency(destinationWallet.getCurrency())
+                .description(description)
+                .timestamp(savedTransaction.getTimestamp())
+                .build();
+
+        transactionProducer.sendTransactionEvent(transactionEvent);
+
+        return TransactionResponseDTO.builder()
+                .transactionId(savedTransaction.getId())
+                .senderEmail(senderEmail)
+                .receiverEmail(receiverEmail)
+                .amount(savedTransaction.getAmount())
+                .status(savedTransaction.getStatus())
+                .type(savedTransaction.getType())
+                .timestamp(savedTransaction.getTimestamp())
+                .build();
+    }
+
+    @Transactional
+    public TransactionResponseDTO recordWithdrawal(Wallet wallet , BigDecimal amount , String description , String senderEmail) {
+        Transaction transaction = Transaction.builder()
+                .amount(amount)
+                .status(TransactionStatus.SUCCESS) // For now
+                .sourceWallet(wallet)
+                .destinationWallet(null)
+                .description(description)
+                .type(TransactionType.WITHDRAWAL)
+                .build();
+
+        Transaction savedTransaction = transactionRepository.save(transaction);
+
+        TransactionEvent transactionEvent = TransactionEvent.builder()
+                .transactionId(savedTransaction.getId())
+                .sourceWalletId(wallet.getId())
+                .destinationWalletId(null)
+                .type(TransactionType.WITHDRAWAL.toString())
+                .amount(amount)
+                .currency(wallet.getCurrency())
+                .description(description)
+                .timestamp(savedTransaction.getTimestamp())
+                .build();
+
+        transactionProducer.sendTransactionEvent(transactionEvent);
+
+        return TransactionResponseDTO.builder()
+                .transactionId(savedTransaction.getId())
+                .senderEmail(senderEmail)
+                .receiverEmail(null)
+                .amount(savedTransaction.getAmount())
+                .status(savedTransaction.getStatus())
+                .type(savedTransaction.getType())
+                .timestamp(savedTransaction.getTimestamp())
+                .build();
+    }
 }
 
