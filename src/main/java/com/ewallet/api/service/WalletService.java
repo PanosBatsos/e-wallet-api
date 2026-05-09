73b7db1453f8ddcae436b789b4b91dcb3a7a9fa3
Panos Batsos <panagiotismpatsos@gmail.com>
2026-05-09 15:55:36 +0300

    feat(WalletService): add logic for all wallet transaction types

--- src/main/java/com/ewallet/api/service/WalletService.java
+++ src/main/java/com/ewallet/api/service/WalletService.java
@@ -5,8 +5,7 @@ package com.ewallet.api.service;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
-import com.ewallet.api.dto.wallet.WalletTransferRequestDTO;
-import com.ewallet.api.dto.wallet.WalletTransferResponseDTO;
+import com.ewallet.api.dto.wallet.*;
 import com.ewallet.api.exception.CurrencyMismatchException;
 import com.ewallet.api.exception.ResourceNotFoundException;
 import com.ewallet.api.repository.WalletRepository;
@@ -14,8 +13,6 @@ import com.ewallet.api.service.kafka.TransactionProducer;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import com.ewallet.api.dto.wallet.WalletDepositRequestDTO;
-import com.ewallet.api.dto.wallet.WalletDepositResponseDTO;
 import com.ewallet.api.entity.TransactionType;
 import com.ewallet.api.entity.User;
 import com.ewallet.api.entity.Wallet;
@@ -64,11 +61,12 @@ public class WalletService {
 
         // Record the transaction for the audit trail.
         // If this operation fails the @Transactional will roll back the balance update.
-        transactionService.recordTransaction(wallet,
-            TransactionType.DEPOSIT,
-             dto.getAmount(),
-              dto.getDescription());
-
+        transactionService.recordDeposit(
+                wallet,
+                dto.getAmount(),
+                dto.getDescription(),
+                userEmail
+        );
 
 
         return WalletDepositResponseDTO.builder()
@@ -88,15 +86,15 @@ public class WalletService {
      * There is no transaction recording for now
      *
      * @param dto The transfer request payload
-     * @param senderEmail The email of the authenticated user initiating the transfer
+     * @param userEmail The email of the authenticated user initiating the transfer
      * @return WalletTransferResponseDTO containing the transfer details
      */
 
 
     @Transactional
-    public WalletTransferResponseDTO transfer(WalletTransferRequestDTO dto , String senderEmail) {
+    public WalletTransferResponseDTO transfer(WalletTransferRequestDTO dto , String userEmail) {
 
-        Wallet senderWallet = walletRepository.findByUserEmail(senderEmail).
+        Wallet senderWallet = walletRepository.findByUserEmail(userEmail).
                 orElseThrow(() -> new ResourceNotFoundException("This wallet does not exist"));
         Wallet receiverWallet = walletRepository.findByUserEmail(dto.getReceiverEmail())
                 .orElseThrow(() -> new ResourceNotFoundException("This user does not exist"));
@@ -119,10 +117,18 @@ public class WalletService {
         senderWallet.setBalance(senderWallet.getBalance().subtract(dto.getAmount()));
         receiverWallet.setBalance(receiverWallet.getBalance().add(dto.getAmount()));
 
-        // Transaction logic to be added in the future
+
+        transactionService.recordTransfer(
+                senderWallet,
+                receiverWallet,
+                dto.getAmount(),
+                dto.getDescription(),
+                userEmail,
+                dto.getReceiverEmail()
+        );
 
         return  WalletTransferResponseDTO.builder()
-                .senderEmail(senderEmail)
+                .senderEmail(userEmail)
                 .receiverEmail(dto.getReceiverEmail())
                 .description(dto.getDescription())
                 .amountTransferred(dto.getAmount())
@@ -130,5 +136,35 @@ public class WalletService {
                 .currency(dto.getCurrency())
                 .build();
                 
+    }
+
+    @Transactional
+    public WalletWithdrawalResponseDTO withdrawal(WalletWithdrawalRequestDTO dto , String userEmail) {
+        Wallet wallet = walletRepository.findByUserEmail(userEmail)
+                .orElseThrow(() -> new ResourceNotFoundException("This wallet does not exist"));
+
+        BigDecimal walletBalance =  wallet.getBalance();
+
+        if (walletBalance.compareTo(dto.getAmount()) < 0) {
+            // InsufficientBalanceException will be added in the future
+            throw new RuntimeException("Insufficient funds for this transfer");
+        }
+
+        wallet.setBalance(wallet.getBalance().subtract(dto.getAmount()));
+
+        transactionService.recordWithdrawal(
+                wallet,
+                dto.getAmount(),
+                dto.getDescription(),
+                userEmail
+        );
+
+        return  WalletWithdrawalResponseDTO.builder()
+                .amountWithdrawn(dto.getAmount())
+                .newBalance(wallet.getBalance())
+                .message("Withdrawal completed succesfully")
+                .timestamp(LocalDateTime.now())
+                .currency(wallet.getCurrency())
+                .build();
     }
 }
